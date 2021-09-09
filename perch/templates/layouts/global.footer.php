84b15692ea20336635e1a72bfe8f8d9beb0e7a7a
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-09-09 11:10:41 +0100

    added generic text block

--- perch/templates/layouts/global.footer.php
+++ perch/templates/layouts/global.footer.php
@@ -2,14 +2,9 @@
     <footer class="l-restrict">
         <div class="c-footer__inner">
             <div>
-                <p><a href="/privacy-policy">Privacy Policy</a> &bull; Copyright St Peter's CIC 2021</p>
+                <p><a href="/privacy-policy">Privacy Policy</a> &bull; &copy; Morningside Hotel 2021</p>
             </div>
             <div class="c-footer__right">
-                <div class="c-footer__charity">
-                    <p>St Peter's CIC is a not for profit something or other</p>
-                    <p>Registered in England and Wales</p>
-                    <p>No. 123 456 789</p>
-                </div>
                 <button id="scrollToTop" class="c-button">Top <i class="fas fa-arrow-up"></i></button>
             </div>
             
